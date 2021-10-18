5cafbba63efe2bfd96631dc55db4de10f795fb5b
JeanMieses <araujo0275@gmail.com>
2021-10-17 22:43:10 -0400

    Fixing navbar layout

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,10 +9,9 @@ const Header = (props) => {
  
     return (<header className={classes.header}>
         <Navbar collapseOnSelect expand="md" bg="light" variant="light">
-
-                <Navbar.Brand className="order-md-0 order-2" href="#home">Sneakers</Navbar.Brand>
-                <Navbar.Toggle   className="border-0 order-md-1 order-0" aria-controls="responsive-navbar-nav" />
-                <Button onClick={props.showCart} className={`order-2 d-flex flex-column align-items-end ` + classes.btn} variant="link"> <span className={classes.badget}> {ctx.items.length} </span> <img src={iconCart} alt='cart'/>  </Button>
+                <Navbar.Toggle  className="border-0 order-md-1 " aria-controls="responsive-navbar-nav" />
+                <Navbar.Brand className="order-md-0" href="#home">Sneakers</Navbar.Brand>
+                <Button onClick={props.showCart} className={`order-md-2 d-flex flex-column align-items-end justify-content-end ` + classes.btn} variant="link"> <span className={classes.badget}> {ctx.items.length} </span> <img src={iconCart} alt='cart'/>  </Button>
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto">
                         <Nav.Link href="#features">Collections</Nav.Link>
@@ -24,6 +23,8 @@ const Header = (props) => {
                 </Navbar.Collapse>
             
         </Navbar>
+
+        
 
     </header>)
 }
